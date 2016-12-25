f495f27c580d6f58d13a71918fb296d8590e5cc5
Shimpei Makimoto <makimoto@tsuyabu.in>
2016-12-26 00:36:49 +0900

    Fetch data and show them

--- app.rb
+++ app.rb
@@ -1,7 +1,24 @@
 require 'sinatra/base'
+require 'net/http'
+require 'json'
 
 class App < Sinatra::Base
   get '/' do
-    'yo'
+    @irkit_commands = fetch_irkit_data
+    haml :index
+  end
+
+  get '/execute/:cmd' do
+    redirect '/'
+  end
+
+  private
+  def fetch_irkit_data
+
+    uri = URI.parse(ENV['IRKIT_DATA_URI'])
+    res = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
+      http.get(uri)
+    end
+    JSON.parse(res.body)
   end
 end
